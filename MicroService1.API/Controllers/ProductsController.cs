5371fc07fcb6ff6098938a9e7ac2e29b24689a04
erhan <erhangokceoglu91@gmail.com>
2023-10-10 15:00:43 +0300

    Add project files.

---
+++ MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(AppDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"{_configuration.GetSection("MicroServices")["Microservice2BaseUrl"]}/api/Products");
+            var context = await response.Content.ReadAsStringAsync();
+            return Ok(context);
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+            _dbContext.Products.Add(new Product() { Name = "Kereste" });
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
